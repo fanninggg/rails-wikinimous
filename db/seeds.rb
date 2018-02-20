5cfa814feea540227d2ee2829751793d97cf5990
Ben Fanning fanninggg@gmail.com <fanninggg@gmail.com>
2018-02-20 16:13:57 +0000

    Second task finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+require "faker"
+
+Article.destroy_all
+
+10.times do
+  articles = Article.new(
+    title: Faker::HitchhikersGuideToTheGalaxy.starship,
+    content: Faker::HitchhikersGuideToTheGalaxy.quote,
+  )
+  articles.save!
+end
+
